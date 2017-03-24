6c32c91bb1fa681c68c901bac87166c39d1f1813
admiralkuro <broyouchill@gmail.com>
2017-03-24 14:15:39 -0400

    asteroid added
    asteroid added we now spinzaku

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+    //public fields
+    public float speed;
+
+    //private fields
+    private Rigidbody2D rb;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+
+public class PlayerController : MonoBehaviour
+{
+    // Public fields
+    public float speed;
+    public Boundary boundary;
+
+    // Private fields
+    private Rigidbody2D rb;
+
+    // Runs ONCE, when object is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame (USED FOR PHYSICS)
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector2(Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax), Mathf.Clamp(rb.position.y, boundary.yMin, boundary.yMax));
+    }
+}
